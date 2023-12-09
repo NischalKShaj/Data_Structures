6aa02b04afeca399d227097f27b3667d92e968cb
nischal <nischalkshaj5@gmail.com>
2023-12-09 15:54:17 +0530

    setting the array

---
+++ Array/setting_array.js
@@ -0,0 +1,13 @@
+// ======================= file to show how to set the values in the array <====================================
+
+// method 1
+let arr = [];
+arr[0] = val1;
+arr[1] = val2;
+
+// method 2
+let arr1 = [val1, val2];
+
+// method 3
+let arr2 = [];
+arr[5] = val; // here the first 4 index that is from 0-4 there is empty space whereas at the 5th position the val will be entered
