5391ac54860be2a98aa5159e662a6dbb7a8019f8
nischal <nischalkshaj5@gmail.com>
2023-12-22 15:34:25 +0530

    new file added

---
+++ reveiw .js
@@ -0,0 +1,17 @@
+let sort = function(arr){
+    for(let i = 0;i<arr.length-1;i++){
+        let min = i;
+        for(j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[min]){
+                min =j
+            }
+        }
+        let temp = arr[min]
+        arr[min] = arr[i]
+        arr[i] = temp;
+    }
+    return arr;
+}
+const arr= [16,7,4,7,4,2]
+const result = sort(arr)
+console.log(result);
