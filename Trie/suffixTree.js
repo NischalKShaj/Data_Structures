237847b74f034b669a02b4b6f4ca6975d826f82b
nischal <nischalkshaj5@gmail.com>
2024-01-01 18:27:59 +0530

    added new datastructure trie

---
+++ Trie/suffixTree.js
@@ -0,0 +1,38 @@
+// =================> file to show the suffix tree <================== //
+
+class Node {
+  constructor() {
+    this.children = {};
+    this.start = null;
+    this.end = null;
+  }
+}
+class SuffixTree {
+  constructor(text) {
+    this.text = text;
+    this.root = new Node();
+    this.buildTree();
+  }
+  buildTree() {
+    for (let i = 0; i < this.text.length; i++) {
+      this.insertSuffix(this.text.substring(i), i);
+    }
+  }
+  insertSuffix(suffix, index) {
+    let node = this.root;
+    for (let i = 0; i < suffix.length; i++) {
+      const char = suffix[i];
+      if (!node.children[char]) {
+        node.children[char] = new Node();
+      }
+      node = node.children[char];
+    }
+    if (node.start === null) {
+      node.start = index;
+      node.end = this.text.length - 1;
+    }
+  }
+}
+const text = "banana";
+const suffixTree = new SuffixTree(text);
+console.log(suffixTree);
